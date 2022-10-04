69a5e6be4f6ad718ab838d740dc420a74fd4abe8
LeaBryant <ldbryant3220@gmail.com>
2022-10-04 11:53:45 -0700

    Update the auth middleware function to work with the GraphQL API.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,8 +5,12 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+    signToken: function ({ username, email, _id }) {
+      const payload = { username, email, _id };
+      return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -19,6 +23,10 @@ module.exports = {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
+    if (!token) {
+      return req;
+    }
+
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
@@ -29,11 +37,6 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return req;
   },
 };
